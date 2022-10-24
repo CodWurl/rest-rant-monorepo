8b113b1e573232191a00ae21d731a874db1509d0
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-10-23 23:27:41 -0400

    save user role

---
+++ backend/migrations/20221024032639-add-user-role.js
@@ -0,0 +1,19 @@
+//Mitigation contents
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'role', {
+        type: Sequelize.DataTypes.ENUM,
+        values: [
+          'reviewer',
+          'admin',
+        ],
+        defaultValue: 'reviewer'
+      })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'role')
+  }
+};
